211b1df4a462b150d4364bf9901a5a3c09cf517b
Arjun Penemetsa <razar87@gmail.com>
2015-10-04 21:31:09 -0400

    initial commit

---
+++ cloudTV/Tab.swift
@@ -0,0 +1,64 @@
+//
+//  Tab.swift
+//  cloudTV
+//
+//  Created by Arjun Penemetsa on 10/3/15.
+//  Copyright © 2015 Arjun Penemetsa. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Tab: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    var collectionCellReuseIdentifier: String = "collectionCell"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+
+
+        if collectionView.tag == 1 {
+            return CGSizeMake(view.frame.size.width, collectionView.frame.size.height + 56)
+            
+        }
+        return CGSizeMake(100,150)
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(collectionCellReuseIdentifier, forIndexPath: indexPath)
+//        cell.backgroundColor = UIColor.blueColor()
+        if collectionView.tag == 1 {
+//        cell.frame.size = CGSizeMake(600, 100)
+            print(cell.frame.size)
+        let a = cell.viewWithTag(2) as! UIImageView
+        let idx = indexPath.item + 1
+//        print(cell.layoutMargins)
+//        print(idx)
+        a.image = UIImage(named: "r\(idx)")
+
+        }
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView.tag == 1 {
+            return 5
+        }
+        else {
+        return 200
+        }
+        
+    }
+    
+    
+  
+    
+}
---
+++ cloudTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cloudTV
+//
+//  Created by Arjun Penemetsa on 10/3/15.
+//  Copyright © 2015 Arjun Penemetsa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
